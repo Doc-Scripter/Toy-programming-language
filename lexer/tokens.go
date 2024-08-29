605b9bb5c55c846dd5d73e847655c3ae4ea996ea
oumouma <garveyshah1595@gmail.com>
2024-08-29 17:41:48 +0300

    feat: token definition

---
+++ lexer/tokens.go
@@ -0,0 +1,68 @@
+package lexer
+
+// Token represents a token in the source code.
+type Token struct {
+	Type  string // A type for different token categories.
+	Value string
+}
+
+type Lexer struct {
+	input        string
+	position     int
+	readPosition int
+	ch           byte
+}
+
+// Different token types
+const (
+	// Special Tokens:
+	ILLEGAL = "ILLEGAL" // Used when the lexer encounters a character or sequence it doesn't recognize
+	EOF     = "EOF"     // "End of file" - Indicates the lexer has reached the end of the input
+
+	// Identifiers and Literals:
+	IDENT = "IDENT" // Used for identifiers, such as variable names or functions
+	INT   = "INT"   // Represents integer literals: sequences of digits.
+
+	// Operators:
+	ASSIGN   = "ASSIGN" // Assignment operator, used for assigning values to variables.
+	PLUS     = "PLUS"   // Used for adding numbers
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+
+	// Comparison Operators;
+	EQ     = "=="
+	NOT_EQ = "!="
+	LT     = "<"
+	GT     = ">"
+
+	// Delimiters:
+	COMMA     = "," // Separates elements, such as parameters in function calls
+	SEMICOLON = ";" // Marks the end of statements
+	LPAREN    = "(" // Opening Parenthesis
+	RPAREN    = ")" // Closing
+	LBRACE    = "{" // Opening curly Braces
+	RBRACE    = "}" // Closing Curly Braces
+
+	// KeyWords
+	FUNCTION = "FUNCTION"
+	VAR      = "VAR"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
+	FOR      = "FOR"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+)
+
+// Maps keywords strings to their respective token types
+var KeywordsMap = map[string]string{
+	"func":   FUNCTION,
+	"var":    VAR,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
+	"for":    FOR,
+	"true":   TRUE,
+	"false":  FALSE,
+}
