728b54d5c596a5655e8f61103ebe3fb2cdab9a03
oumouma <garveyshah1595@gmail.com>
2024-09-17 21:55:32 +0300

    git refactor code - rearranged the code structure improve code readability

--- ast/ast.go
+++ ast/ast.go
@@ -17,18 +17,24 @@ type Program struct {
 	Statements []Statement
 }
 
-// interface representing a statement
-type Statement interface {
-	Node
-	statementNode()
-}
-
 func (p *Program) TokenLiteral() string {
 	if len(p.Statements) > 0 {
 		return p.Statements[0].TokenLiteral()
 	} else {
 		return ""
 	}
+}
+
+// interface representing a statement
+type Statement interface {
+	Node
+	statementNode()
+}
+
+// is an interface for all expressions
+type Expression interface {
+	Node
+	expressionNode()
 }
 
 // represents a variable declaration(var x = 5)
@@ -38,19 +44,26 @@ type VarStatement struct {
 	Value Expression
 }
 
+func (vs *VarStatement) statementNode() {}
+func (vs *VarStatement) TokenLiteral() string {
+	if vs.Token.Literal == "" {
+		return "nil"
+	}
+	return vs.Token.Literal
+}
+
 // represents variable names
 type Identifier struct {
 	Token token.Token // The token.IDENT token
 	Value string
 }
 
-func (i *Identifier) expressionNode()      {}
-func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
-
-// is an interface for all expressions
-type Expression interface {
-	Node
-	expressionNode()
+func (i *Identifier) expressionNode() {}
+func (i *Identifier) TokenLiteral() string {
+	if i.Token.Literal == "" {
+		return "nil"
+	}
+	return i.Token.Literal
 }
 
 // represents interger values
@@ -58,6 +71,3 @@ type IntegerLiteral struct {
 	Token token.Token // The token.INT token
 	Value int64
 }
-
-func (ls *VarStatement) statementNode()       {}
-func (ls *VarStatement) TokenLiteral() string { return ls.Token.Literal }
