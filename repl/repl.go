0dc4c9879cc80b6a32b4a206523a9881c62c0f5c
oumouma <garveyshah1595@gmail.com>
2024-09-25 06:48:08 +0300

    feat:  add logic to print the ast on the CLI

--- repl/repl.go
+++ repl/repl.go
@@ -4,9 +4,10 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"os"
 
 	"ksm/lexer"
-	"ksm/token"
+	"ksm/parser"
 )
 
 const REPL_PROMPT = "\033[32mEnter Input:\033[0m "
@@ -25,10 +26,28 @@ func StartRepl(input io.Reader, output io.Writer) {
 		line := scanner.Text()
 
 		L := lexer.New(line)
+		p := parser.New(L)
 
-		for tok := L.NextToken(); tok.Type != token.EOF; tok = L.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		program := p.ParseProgram()
+		checkParserErrors(p)
+
+		for _, brnch := range program.Statements {
+			fmt.Printf("%+++v\n", brnch)
 		}
 		fmt.Println()
 	}
+}
+
+func checkParserErrors(p *parser.Parser) {
+	errors := p.Errors()
+
+	if len(errors) == 0 {
+		return
+	}
+
+	fmt.Printf("parser has %d errors", len(errors))
+	for _, msg := range errors {
+		fmt.Printf("parser error: %q", msg)
+	}
+	os.Exit(1)
 }
